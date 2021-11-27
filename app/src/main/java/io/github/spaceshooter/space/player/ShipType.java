c1f5a25f764a2dd0c7a7b34927d7ba0de9473996
Gael Rial Costas <g.rial.2018@alumnos.urjc.es>
2021-11-27 16:46:55 +0100

    Add heavy ship.

--- app/src/main/java/io/github/spaceshooter/space/player/ShipType.java
+++ app/src/main/java/io/github/spaceshooter/space/player/ShipType.java
@@ -7,7 +7,7 @@ public enum ShipType {
 
     NORMAL(1, 100, 0.1f, 5, R.drawable.ship_black, R.drawable.ship_red),
     FAST(1.5f, 70, 0.05f, 3, R.drawable.ship_light_black, R.drawable.ship_light_red),
-    TANK(0.7f, 200, 0.25f, 15, R.drawable.ship_black, R.drawable.ship_red);
+    TANK(0.7f, 200, 0.25f, 15, R.drawable.ship_heavy_black, R.drawable.ship_heavy_red);
 
     private final float speed;
     private final int maxHealth;
